16c9ce7db5c5c0fac76ac1129bd966728e79f7d1
Allen Casey <36112600+allenmcasey@users.noreply.github.com>
2021-03-24 20:04:44 -0500

    initial commit

---
+++ start_servers.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+python3 server.py 100 1 10007 &
+python3 server.py 101 1 10008 &
+python3 server.py 102 1 10009 &
+python3 server.py 103 2 10010 &
+python3 server.py 104 2 10011 &
+python3 server.py 105 3 10012 &
---
+++ test_clients.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+python3 client.py 100 10000 client_packet_1.json &
+python3 client.py 100 10000 client_packet_1.json &
+python3 client.py 100 10000 client_packet_1.json &
+python3 client.py 100 10000 client_packet_2.json &
+python3 client.py 100 10000 client_packet_1.json &
+python3 client.py 100 10000 client_packet_2.json &
+python3 client.py 100 10000 client_packet_1.json &
+python3 client.py 100 10000 client_packet_3.json &
+python3 client.py 100 10000 client_packet_3.json &
+python3 client.py 100 10000 client_packet_3.json &
+python3 client.py 100 10000 client_packet_1.json &
+python3 client.py 100 10000 client_packet_1.json &
+python3 client.py 100 10000 client_packet_1.json &
+python3 client.py 100 10000 client_packet_1.json &
+python3 client.py 100 10000 client_packet_1.json &
+python3 client.py 100 10000 client_packet_2.json &
+python3 client.py 100 10000 client_packet_1.json &
+python3 client.py 100 10000 client_packet_2.json &
+python3 client.py 100 10000 client_packet_1.json &
+python3 client.py 100 10000 client_packet_3.json &
+python3 client.py 100 10000 client_packet_3.json &
+python3 client.py 100 10000 client_packet_3.json &
+python3 client.py 100 10000 client_packet_1.json &
+python3 client.py 100 10000 client_packet_1.json &
+python3 client.py 100 10000 client_packet_3.json &
+python3 client.py 100 10000 client_packet_3.json &
+python3 client.py 100 10000 client_packet_3.json &
+python3 client.py 100 10000 client_packet_2.json &
+python3 client.py 100 10000 client_packet_2.json &
+python3 client.py 100 10000 client_packet_3.json &
+python3 client.py 100 10000 client_packet_3.json &
+python3 client.py 100 10000 client_packet_1.json &
+python3 client.py 100 10000 client_packet_3.json &
+python3 client.py 100 10000 client_packet_1.json &
+python3 client.py 100 10000 client_packet_3.json &
+python3 client.py 100 10000 client_packet_3.json
