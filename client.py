a7171b67c3b08b1ad8e8ad864162f7d1d6079f6f
Allen Casey <36112600+allenmcasey@users.noreply.github.com>
2021-04-04 14:24:53 -0500

    add check for packet payload size

--- client.py
+++ client.py
@@ -9,21 +9,29 @@ localhost = '127.0.0.1'
 rp_port = int(sys.argv[2])
 json_filename = sys.argv[3]
 
-# create connection to reverse proxy
-rp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-rp_socket.connect((localhost, rp_port))
-
 while True:
 
-    # read/serialize message, then send/receive
+    # read message
     json_file = open(json_filename, "r")
     json_file_data = json.load(json_file)
+
+    # check payload size
+    if len(json_file_data['payload']) > 1024:
+        print("Packet payload is too large, please send a message smaller than 1024 bytes.")
+        break
+
+    # create connection to reverse proxy
+    rp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    rp_socket.connect((localhost, rp_port))
+
+    # serialize message, then send/receive
     rp_socket.send(pickle.dumps(json_file_data))
     json_received_data = pickle.loads(rp_socket.recv(1024))
 
     # check if this is an error response
     if json_received_data["type"] == -1:
         print("Requested policy not found, please try again later...")
+        rp_socket.close()
         break
 
     # get expected/received hashes
@@ -33,12 +41,13 @@ while True:
     # test validity of hash
     if received_hash == expected_hash:
         print("Transmission successfully received by server")
+        rp_socket.close()
         break
     else:
         user_answer = input("Transmission to server was corrupted. Do you want to try again? (y/n): ")
+        rp_socket.close()
         if user_answer == 'y':
             continue
         else:
             break
 
-rp_socket.close()
