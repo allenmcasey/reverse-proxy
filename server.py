2282678f881c7f6c975b888356c92cdb1c714d44
Allen Casey <36112600+allenmcasey@users.noreply.github.com>
2021-04-15 22:43:10 -0500

    add changes to make more EC2-instance-friendly

--- server.py
+++ server.py
@@ -24,7 +24,6 @@ def handle_connection(client):
 
 def Main():
 
-    localhost = "127.0.0.1"
     reverse_proxy_port = 10000
     this_id = int(sys.argv[1])
     this_policy = int(sys.argv[2])
@@ -32,13 +31,13 @@ def Main():
 
     # create socket to listen on
     in_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    in_socket.bind((localhost, this_port))
+    in_socket.bind(("", this_port))
     in_socket.listen(6)
     print("server socket listening... bound to port", this_port)
 
     # create socket with reverse proxy
     reverse_proxy_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    reverse_proxy_socket.connect((localhost, reverse_proxy_port))
+    reverse_proxy_socket.connect(("3.20.189.253", reverse_proxy_port))
 
     # create a setup packet
     setup_json_data = {
